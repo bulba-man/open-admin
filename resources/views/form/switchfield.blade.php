f6f04ce8a1dfe96b31e60fe2cfed8879c64094ca
Mikita Dzmitryievich <nikitadm@tut.by>
2025-03-28 01:06:46 +0300

    change switch values from on/off to 1/0

--- resources/views/form/switchfield.blade.php
+++ resources/views/form/switchfield.blade.php
@@ -2,7 +2,7 @@
 
     <div class="form-check form-switch">
         <input type="hidden" name="{{$name}}" id="{{$id}}" value="{{ old($column, $value) }}" />
-        <input class="form-check-input {{$class}}" name="{{$name}}_cb" type="checkbox" id="{{$name}}_cb" {{ !empty(old($column, $value)) ? 'checked' : '' }} {!! $attributes !!} onchange="document.querySelector('#{{$id}}').value = (this.checked ? 'on' : 'off')" />
+        <input class="form-check-input {{$class}}" name="{{$name}}_cb" type="checkbox" id="{{$name}}_cb" {{ !empty(old($column, $value)) ? 'checked' : '' }} {!! $attributes !!} onchange="document.querySelector('#{{$id}}').value = (this.checked ? 1 : 0)" />
     </div>
 
 @include("admin::form._footer")
