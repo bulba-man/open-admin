d40458d8e6df6b3e34b4fe5c45443ba66d17283c
Mikita Dzmitryievich <nikitadm@tut.by>
2025-04-03 23:41:24 +0300

    add values method to switch field

--- src/Form/Field/SwitchField.php
+++ src/Form/Field/SwitchField.php
@@ -11,18 +11,42 @@ class SwitchField extends Field
     use CanCascadeFields;
 
     protected $cascadeEvent = 'change';
-    
+
+    protected $options = [
+        'on' => '1',
+        'off' => '0',
+    ];
+
     public function prepare($value)
     {
-        if ($value == 'on' || $value == 1) {
-            $value = 1;
-        } elseif ($value == 'off' || $value === '0') {
-            $value = 0;
-        } else {
-            $value = false; // nothting was set so do: false to ignore value from saving
+        $value = trim($value);
+
+        if (strtolower($value) === 'true' || strtolower($value) === 'false') {
+            $value = filter_var(   $value, FILTER_VALIDATE_BOOLEAN);
+        } elseif (is_numeric($value)) {
+            $value = filter_var(   $value, FILTER_VALIDATE_INT);
         }
 
         return $value;
+    }
+
+    public function values(string $on = '1', string $off = '0'): static
+    {
+        $this->options = [
+            'on' => $on,
+            'off' => $off,
+        ];
+
+        return $this;
+    }
+
+    public function variables(): array
+    {
+        $this->variables = array_merge($this->variables, [
+            'options'         => $this->options
+        ]);
+
+        return parent::variables();
     }
 
     public function render()
