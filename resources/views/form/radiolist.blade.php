65355ffdcd9011558bcfd1eb589ff9ee1876a91a
Mikita Dzmitryievich <nikitadm@tut.by>
2025-05-26 15:00:29 +0300

    radiolist: remove default label class

--- resources/views/form/radiolist.blade.php
+++ resources/views/form/radiolist.blade.php
@@ -7,7 +7,7 @@
             @foreach($options as $option => $label)
                 <li class="list-group-item">
                     <input type="radio" name="{{$name}}" value="{{$option}}" id="{{$name}}-{{$option}}" class="btn-check {{$class}}" {{ ($option == old($column, $value)) || ($value === null && in_array($label, $checked)) ?'checked':'' }} {!! $attributes !!} />
-                    <label class="btn btn-outline-primary" for="{{$name}}-{{$option}}">{{$label}}</label>
+                    <label class="btn" for="{{$name}}-{{$option}}">{{$label}}</label>
                 </li>
             @endforeach
             </ul>
