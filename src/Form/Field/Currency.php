976a72b4164bc64fafd79a9ebb45df283382c5d6
Mikita Dzmitryievich <dmitrievichn@gmail.com>
2026-06-14 00:38:18 +0300

    remove max-width style from currency field

--- src/Form/Field/Currency.php
+++ src/Form/Field/Currency.php
@@ -72,7 +72,6 @@ class Currency extends Text
         $this->inputmask($this->options);
 
         $this->prepend($this->symbol);
-        $this->style('max-width', '160px');
 
         return parent::render();
     }
